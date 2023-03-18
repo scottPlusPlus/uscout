a47c8dc5dd14fa5e650942828a87c33252833472
scottPlusPlus <mail@scottplusplus.com>
2023-03-18 15:42:07 -0700

    returning cached pages immediately instead of waiting to re-fetch

--- app/code/RequestInfo.ts
+++ app/code/RequestInfo.ts
@@ -3,51 +3,56 @@ import UInfoModel from "~/models/uinfo.server";
 import scrapePage from "./ScrapePage.server";
 import { twentyFourHoursAgo } from "./timeUtils";
 
-import * as createError from 'http-errors';
+import * as createError from "http-errors";
 import { sanitizeUrl } from "./urlUtils";
 
-
 export async function requestSingle(url: string): Promise<UInfo> {
-    const sanitizedUrl = sanitizeUrl(url);
-    if (!sanitizedUrl){
-        return Promise.reject(createError.BadRequest("Invalid URL"));
-    }
+  const sanitizedUrl = sanitizeUrl(url);
+  if (!sanitizedUrl) {
+    return Promise.reject(createError.BadRequest("Invalid URL"));
+  }
 
-    const now = new Date();
+  const now = new Date();
 
-    const existing = await UInfoModel.get(sanitizedUrl);
-    if (existing){
-        console.log(sanitizedUrl + " already exists");
-        if (existing.checked > twentyFourHoursAgo() ){
-            console.log("returning cached info for " + sanitizedUrl);
-            return existing;
-        }
-        console.log("but I guess was too old?");
+  const existing = await UInfoModel.get(sanitizedUrl);
+  if (existing) {
+    console.log(sanitizedUrl + " already exists");
+    if (existing.checked < twentyFourHoursAgo()) {
+      console.log("but too old, triggering scrape");
+      scrapeAndSavePage(sanitizedUrl);
     }
+    console.log("returning cached info for " + sanitizedUrl);
+    return existing;
+  }
 
-    const scrape = await scrapePage(sanitizedUrl);
-    const newInfo:UInfo = {
-        url: sanitizedUrl,
-        fullUrl: scrape.url,
-        hash: scrape.hash,
-        title: scrape.title,
-        summary: scrape.summary,
-        image: scrape.image || "",
-        contentType: scrape.contentType || null,
-        duration: scrape.duration || null,
-        likes: scrape.likes || null,
-        authorName: scrape.authorName || null,
-        authorLink: scrape.authorLink || null,
-        created: now,
-        updated: now,
-        checked: now,
-    }
-    return await UInfoModel.set(newInfo);
+  return await scrapeAndSavePage(sanitizedUrl);
+}
+
+async function scrapeAndSavePage(url: string): Promise<UInfo> {
+  const scrape = await scrapePage(url);
+  const now = new Date();
+  const newInfo: UInfo = {
+    url: url,
+    fullUrl: scrape.url,
+    hash: scrape.hash,
+    title: scrape.title,
+    summary: scrape.summary,
+    image: scrape.image || "",
+    contentType: scrape.contentType || null,
+    duration: scrape.duration || null,
+    likes: scrape.likes || null,
+    authorName: scrape.authorName || null,
+    authorLink: scrape.authorLink || null,
+    created: now,
+    updated: now,
+    checked: now,
+  };
+  return await UInfoModel.set(newInfo);
 }
 
 export function requestMany(urls: string[]): Promise<UInfo[]> {
-    const promises = urls.map(u => {
-        return requestSingle(u);
-    });
-    return Promise.all(promises);
+  const promises = urls.map((u) => {
+    return requestSingle(u);
+  });
+  return Promise.all(promises);
 }
