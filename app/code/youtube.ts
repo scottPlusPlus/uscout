376ff61284eee2a6e76d20185cc0cef3b7f635cc
Nick Imkamp <imkampn@gmail.com>
2023-03-30 07:57:08 -0500

    Format youtube file with prettier

--- app/code/youtube.ts
+++ app/code/youtube.ts
@@ -1,61 +1,61 @@
-async function scrapeYouTubeVideoImpl (videoId: string) {
-  const YOUTUBE_API_KEY = process.env.YOUTUBE_API_KEY
-  const apiUrl = `https://www.googleapis.com/youtube/v3/videos?id=${videoId}&part=snippet,statistics&key=${YOUTUBE_API_KEY}`
-  const response = await fetch(apiUrl)
-  const data = await response.json()
-  const video = data.items[0]
+async function scrapeYouTubeVideoImpl(videoId: string) {
+  const YOUTUBE_API_KEY = process.env.YOUTUBE_API_KEY;
+  const apiUrl = `https://www.googleapis.com/youtube/v3/videos?id=${videoId}&part=snippet,statistics&key=${YOUTUBE_API_KEY}`;
+  const response = await fetch(apiUrl);
+  const data = await response.json();
+  const video = data.items[0];
   // const duration = video.contentDetails.duration;
-  const likes = video.statistics.likeCount
-  const authorName = video.snippet.channelTitle
-  const authorLink = `https://www.youtube.com/channel/${video.snippet.channelId}`
-  const contentType = 'Video'
+  const likes = video.statistics.likeCount;
+  const authorName = video.snippet.channelTitle;
+  const authorLink = `https://www.youtube.com/channel/${video.snippet.channelId}`;
+  const contentType = "Video";
   return {
     // duration,
     likes,
     authorName,
     authorLink,
     contentType
-  }
+  };
 }
 
-export function isYouTubeVideo (url: string) {
+export function isYouTubeVideo(url: string) {
   // Match YouTube watch URL format
-  const watchPattern = /youtube\.com\/watch\?v=([a-zA-Z0-9_-]+)/
+  const watchPattern = /youtube\.com\/watch\?v=([a-zA-Z0-9_-]+)/;
 
   // Match YouTube short URL format
-  const shortPattern = /youtu\.be\/([a-zA-Z0-9_-]+)/
+  const shortPattern = /youtu\.be\/([a-zA-Z0-9_-]+)/;
 
-  return watchPattern.test(url) || shortPattern.test(url)
+  return watchPattern.test(url) || shortPattern.test(url);
 }
 
-export function getVideoIdFromUrl (url: string) {
-  const regex = /(?:\?v=|\/embed\/|\/watch\?v=|\/\w+\/\w+\/)([\w-]{11})/
-  const match = url.match(regex)
-  console.log('Match: ', match)
+export function getVideoIdFromUrl(url: string) {
+  const regex = /(?:\?v=|\/embed\/|\/watch\?v=|\/\w+\/\w+\/)([\w-]{11})/;
+  const match = url.match(regex);
+  console.log("Match: ", match);
   if (match) {
-    return match[1]
+    return match[1];
   }
-  return null
+  return null;
 }
 
-export async function scrapeYouTubeVideo (urlStr: string) {
+export async function scrapeYouTubeVideo(urlStr: string) {
   if (isYouTubeVideo(urlStr)) {
-    let videoId = getVideoIdFromUrl(urlStr)
+    let videoId = getVideoIdFromUrl(urlStr);
     if (videoId === null) {
-      return null
+      return null;
     }
-    console.log('VideoID: ', videoId)
-    const scrapedVideoContent = await scrapeYouTubeVideoImpl(videoId)
-    console.log('Scraped video Content: ', scrapedVideoContent)
+    console.log("VideoID: ", videoId);
+    const scrapedVideoContent = await scrapeYouTubeVideoImpl(videoId);
+    console.log("Scraped video Content: ", scrapedVideoContent);
 
-    const contentType = scrapedVideoContent.contentType
-    const authorLink = scrapedVideoContent.authorLink
-    const likes = scrapedVideoContent.likes
-    const authorName = scrapedVideoContent.authorName
-    console.log('Content Type: ', contentType)
-    console.log('Author Link: ', authorLink)
-    console.log('Likes: ', likes)
-    console.log('Author Name: ', authorName)
+    const contentType = scrapedVideoContent.contentType;
+    const authorLink = scrapedVideoContent.authorLink;
+    const likes = scrapedVideoContent.likes;
+    const authorName = scrapedVideoContent.authorName;
+    console.log("Content Type: ", contentType);
+    console.log("Author Link: ", authorLink);
+    console.log("Likes: ", likes);
+    console.log("Author Name: ", authorName);
 
     return {
       contentType,
@@ -63,6 +63,6 @@ export async function scrapeYouTubeVideo (urlStr: string) {
       likes,
       // dislikes,
       authorName
-    }
+    };
   }
 }
