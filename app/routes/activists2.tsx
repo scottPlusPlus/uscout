847d890eb3c138e9367c3d2c6381cf1e2f45c52e
Nick Imkamp <imkampn@gmail.com>
2023-07-09 12:32:22 -0500

    Remove empty space

--- app/routes/activists2.tsx
+++ app/routes/activists2.tsx
@@ -80,7 +80,6 @@ export default function ActivistsPage() {
   const loadedItems = itemsFromRemixData(data.collectionItems, infoMap);
 
   return (
-    
     <PageWithSections sections={sections} infos={uInfos} ipab={data.ipab} collectionItems={loadedItems}/>
   )
 
