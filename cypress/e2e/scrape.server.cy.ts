0c80a22dc2be89d11c522780deb0fb3fc4451c09
Nick Imkamp <imkampn@gmail.com>
2023-07-16 13:20:27 -0500

    Add test to make sure that the returned time stamp is not too far into the future.

--- cypress/e2e/scrape.server.cy.ts
+++ cypress/e2e/scrape.server.cy.ts
@@ -29,6 +29,34 @@ describe("getLatestSnapshot", () => {
       });
   });
 
+  it("should ensure that the return timestamp from wayback machine is not too far into the future", () => {
+    const identifier = "youtube.com";
+
+    cy.get('input[name="url"]', { timeout: 5000 })
+      .should("be.visible")
+      .then(($input) => {
+        $input.val(identifier);
+      });
+
+    cy.get('button[type="submit"]', { timeout: 5000 })
+      .should("exist")
+      .should("be.visible")
+      .click({ force: true });
+
+    cy.get('[data-test="jsonDisplay"]')
+      .should("exist")
+      .invoke("text")
+      .then((jsonText) => {
+        const jsonData = JSON.parse(jsonText);
+        expect(jsonData.info.timeUpdatedSource).to.equal("archive.org");
+        const timeStampDate = new Date(jsonData.info.timeUpdated * 1000);
+
+        // Make sure timestamp is not more than 1 hour into the future
+        const oneHourIntoFuture = new Date(Date.now() + 60 * 60 * 1000);
+        expect(timeStampDate <= oneHourIntoFuture).to.be.true;
+      });
+  });
+
   it('should display "Last Updated and Time Updated Source from twitters " with the correct data when the request is successful', () => {
     const identifier = "espn.com";
 
