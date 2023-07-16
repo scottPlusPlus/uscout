109321870927f21b379a0a8c0481c57ea2c1ace7
Nick Imkamp <imkampn@gmail.com>
2023-07-16 11:34:38 -0500

    Change function definition to return a promise that resolves with a string when successful or returns null if unsucessful. Remove commented out code that isn't being used anymore.

--- app/code/archive.ts
+++ app/code/archive.ts
@@ -1,38 +1,6 @@
 const axios = require("axios");
 
-// export async function getArchiveOrgIdentifier(url: string): Promise<string> {
-//   console.log("Getting Identifier");
-//   // Extract the domain name from the URL
-//   const urlObj = new URL(url);
-//   const domainName = urlObj.hostname;
-//   console.log("Domain Name: ", domainName);
-
-//   // Call the Wayback Machine API to get the latest capture of the website
-//   const apiUrl = `https://archive.org/wayback/available?url=${domainName}&timestamp=`;
-
-//   const response = await fetch(apiUrl + Date.now());
-
-//   if (!response.ok) {
-//     throw new Error(`Failed to fetch Wayback Machine API: ${response.status}`);
-//   }
-
-//   const data = await response.json();
-//   console.log("Data: ", data);
-
-//   // Check if there is an archive available for the website
-//   if (!data.archived_snapshots || !data.archived_snapshots.closest) {
-//     throw new Error(`No archive available for ${url}`);
-//   }
-
-//   // Return the archive.org identifier
-//   const archiveMetaData = data.archived_snapshots.closest.url.split("/");
-//   console.log("Array: ", archiveMetaData);
-//   archiveMetaData.pop();
-//   console.log("Array2: ", archiveMetaData);
-//   return archiveMetaData.pop();
-// }
-
-export async function getLatestSnapshot(url: string) {
+export async function getLatestSnapshot(url: string): Promise<string | null> {
   console.log("Identifier: ", url);
   const requestUrl = `https://archive.org/wayback/available?url=${url}`;
 
@@ -45,9 +13,14 @@ export async function getLatestSnapshot(url: string) {
     });
     const jsonData = response.data;
     console.log("JSON Data: ", jsonData);
-    return jsonData.archived_snapshots.closest.timestamp;
+    if (jsonData?.archived_snapshots?.closest?.timestamp) {
+      return jsonData.archived_snapshots.closest.timestamp;
+    } else {
+      return null;
+    }
   } catch (error) {
     console.log("Error:", error);
+    return null;
   }
 }
 
