c01aae8c66b3709d02a20ca81bd8ada733844e6b
Nick Imkamp <imkampn@gmail.com>
2023-07-16 13:51:03 -0500

    Created a component for the new collection modal

--- app/routes/me.tsx
+++ app/routes/me.tsx
@@ -30,16 +30,6 @@ export default function UserPage() {
     }
   });
 
-  const [modalIsOpen, setModalIsOpen] = useState(false);
-
-  const openModal = () => {
-    setModalIsOpen(true)
-  }
-
-  const closeModal = () => {
-    setModalIsOpen(false)
-  }
-
   return (
     <div className="flex h-full min-h-screen flex-col">
       <header className="flex items-center justify-between bg-slate-800 p-4 text-white">
@@ -47,21 +37,7 @@ export default function UserPage() {
           Hello
         </h1>
         <p>{user.email}</p>
-        <div>
-            <button className="rounded bg-slate-600 py-2 px-4 text-blue-100 hover:bg-blue-500 active:bg-blue-600"
-            onClick={openModal}>New Collection</button>
-            <Modal
-                isOpen={modalIsOpen}
-                onRequestClose={closeModal}
-                contentLabel="New Collection Form"
-            >
-                <NewCollectionPage />
-                <button
-                className="rounded bg-blue-500 py-2 px-4 text-white hover:bg-blue-600 focus:bg-blue-400"
-                onClick={closeModal}>Close</button>
-
-            </Modal>
-        </div>
+        <NewCollectionModal/>
         <Form action="/logout" method="post">
           <button
             type="submit"
@@ -85,4 +61,33 @@ export default function UserPage() {
       </main>
     </div>
   );
+}
+
+const NewCollectionModal = () => {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+
+  const openModal = () => {
+    setModalIsOpen(true)
+  }
+
+  const closeModal = () => {
+    setModalIsOpen(false)
+  }
+
+  return (<div>
+  <button className="rounded bg-slate-600 py-2 px-4 text-blue-100 hover:bg-blue-500 active:bg-blue-600"
+  onClick={openModal}>New Collection</button>
+  <Modal
+      isOpen={modalIsOpen}
+      onRequestClose={closeModal}
+      contentLabel="New Collection Form"
+  >
+      <NewCollectionPage />
+      <button
+      className="rounded bg-blue-500 py-2 px-4 text-white hover:bg-blue-600 focus:bg-blue-400"
+      onClick={closeModal}>Close</button>
+
+  </Modal>
+</div>)
+
 }
