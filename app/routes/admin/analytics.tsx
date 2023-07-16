e203e1c2dcaa2bf2cdf822eb69b970e9aa486d60
Nick Imkamp <imkampn@gmail.com>
2023-07-16 18:24:46 -0500

    Create a table in the analytics component in which counts the number of events + data. Add a column to display that count. Delete entries older than 90 days.

--- app/routes/admin/analytics.tsx
+++ app/routes/admin/analytics.tsx
@@ -61,7 +61,8 @@ export default function AdminPage() {
 
   const countEvents = (events) => {
     return events.reduce((counts, event) => {
-      counts[event.event] = (counts[event.event] || 0) + 1;
+      const key = `${event.event}-${event.data}`;
+      counts[key] = (counts[key] || 0) + 1;
       return counts;
     }, {});
   };
@@ -69,14 +70,36 @@ export default function AdminPage() {
   const renderRows2 = () => {
     console.log("render rows...");
     const eventCounts = countEvents(sortedEvents());
-    return sortedEvents().map((aEvent) => (
-      <tr key={aEvent.id}>
-        <td className="px-4 py-2">{dayjs(aEvent.ts).format('YYYY-MM-DD')}</td>
-        <td className="px-4 py-2">{aEvent.event}</td>
-        <td className="px-4 py-2">{aEvent.data}</td>
-        <td className="px-4 py-2">{eventCounts[aEvent.event]}</td>
-      </tr>
-    ));
+
+    // Create a Set to track unique event and data combinations
+    const uniqueEventAndData = new Set();
+
+    // Filter events that are older than 90 days
+    const currentDate = new Date();
+    const filteredEvents = sortedEvents().filter((aEvent) => {
+      const eventDate = new Date(aEvent.ts);
+      const daysDifference = Math.floor((currentDate.getTime() - eventDate.getTime()) / (1000 * 60 * 60 * 24));
+      return daysDifference <= 90;
+    });
+
+    return filteredEvents.map((aEvent) => {
+      const { event, data } = aEvent;
+      const key = `${event}-${data}`;
+      const isUnique = !uniqueEventAndData.has(key);
+      uniqueEventAndData.add(key);
+
+      // Check if event and data are empty
+      const isEventDataEmpty = !event.trim() && !data.trim();
+
+      return isUnique && !isEventDataEmpty ? (
+        <tr key={aEvent.id}>
+          <td className="px-4 py-2">{dayjs(aEvent.ts).format('YYYY-MM-DD')}</td>
+          <td className="px-4 py-2">{event}</td>
+          <td className="px-4 py-2">{data}</td>
+          <td className="px-4 py-2">{eventCounts[key]}</td>
+        </tr>
+      ) : null;
+    });
   };
 
   return (
