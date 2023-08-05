942886a6c4ecd6b4796cffffdf8177f8c6495313
Nick Imkamp <imkampn@gmail.com>
2023-08-05 09:06:54 -0500

    Add error handling for deleteOldEvents function. Finish tally Analytics by creating that window of data we need to retreive. Add error handling as well for db calls.

--- app/models/analyticEvent.server.ts
+++ app/models/analyticEvent.server.ts
@@ -33,13 +33,18 @@ export async function deleteOldEvents(): Promise<void> {
   const ninetyDaysAgo = new Date();
   ninetyDaysAgo.setDate(ninetyDaysAgo.getDate() - 90);
 
-  await prisma.analyticEvent.deleteMany({
-    where: {
-      ts: {
-        lt: ninetyDaysAgo
+  try {
+    await prisma.analyticEvent.deleteMany({
+      where: {
+        ts: {
+          lt: ninetyDaysAgo
+        }
       }
-    }
-  });
+    });
+  } catch (error) {
+    console.error("Error occurred while deleting 90 day old data: ", error);
+    throw error;
+  }
 }
 
 type AnalyticEventWithCount = AnalyticEvent & { count: number };
@@ -76,77 +81,86 @@ export async function getAnalyticsDataLast7Days(): Promise<Array<ARes>> {
 }
 
 export async function tallyAnalytics(): Promise<Array<ARes>> {
-  const latestTally = await prisma.analyticEventByDay.findFirst({
-    orderBy: {
-      day: "desc"
-    }
-  });
-
-  let startDate;
-
-  if (latestTally) {
-    startDate = new Date(latestTally.day);
-    startDate.setDate(startDate.getDate() + 1);
-  } else {
-    startDate = new Date();
-    startDate.setDate(startDate.getDate() - 2);
-  }
-
-  const analyticsData = await prisma.analyticEvent.findMany({
-    where: {
-      ts: {
-        lt: startDate.toISOString()
-      }
-    }
-  });
-
-  const map = new Map<string, ARes>();
-  analyticsData.forEach((x) => {
-    const key = x.event + x.data;
-    const prev = map.get(key) ?? {
-      event: x.event,
-      data: x.data,
-      count: 0
-    };
-    prev.count += 1;
-    map.set(key, prev);
-  });
-
-  var res = [...map.values()];
-  res = res.sort((a, b) => b.count - a.count);
-
-  const today = new Date();
-  today.setHours(0, 0, 0, 0);
-
-  for (const item of res) {
-    const existingRecord = await prisma.analyticEventByDay.findFirst({
-      where: {
-        event: item.event,
-        data: item.data,
-        day: today
+  try {
+    const latestTally = await prisma.analyticEventByDay.findFirst({
+      orderBy: {
+        day: "desc"
       }
     });
 
-    if (existingRecord) {
-      await prisma.analyticEventByDay.update({
-        where: {
-          id: existingRecord.id
-        },
-        data: {
-          count: existingRecord.count + item.count
-        }
-      });
+    let startDate;
+    let endDate;
+
+    if (latestTally) {
+      endDate = new Date(latestTally.day);
+      startDate = new Date();
+      startDate.setDate(startDate.getDate() - 2);
     } else {
-      await prisma.analyticEventByDay.create({
-        data: {
+      startDate = new Date();
+      startDate.setDate(startDate.getDate() - 2);
+      endDate = new Date(0);
+    }
+
+    const analyticsData = await prisma.analyticEvent.findMany({
+      where: {
+        ts: {
+          lt: startDate.toISOString(),
+          gt: endDate.toISOString()
+        }
+      }
+    });
+
+    const map = new Map<string, ARes>();
+    analyticsData.forEach((x) => {
+      const key = x.event + x.data;
+      const prev = map.get(key) ?? {
+        event: x.event,
+        data: x.data,
+        count: 0
+      };
+      prev.count += 1;
+      map.set(key, prev);
+    });
+
+    var res = [...map.values()];
+    res = res.sort((a, b) => b.count - a.count);
+
+    const today = new Date();
+    today.setHours(0, 0, 0, 0);
+
+    for (const item of res) {
+      const existingRecord = await prisma.analyticEventByDay.findFirst({
+        where: {
           event: item.event,
           data: item.data,
-          count: item.count,
           day: today
         }
       });
-    }
-  }
 
-  return res;
+      if (existingRecord) {
+        await prisma.analyticEventByDay.update({
+          where: {
+            id: existingRecord.id
+          },
+          data: {
+            count: existingRecord.count + item.count
+          }
+        });
+      } else {
+        await prisma.analyticEventByDay.create({
+          data: {
+            event: item.event,
+            data: item.data,
+            count: item.count,
+            day: today
+          }
+        });
+      }
+    }
+
+    return res;
+  } catch (error) {
+    console.error("An error occurred while tallying analytics:", error);
+    throw error;
+  }
 }
