32426c1eb80f9e5c375055397849797852fd6215
Nick Imkamp <imkampn@gmail.com>
2023-08-27 16:52:53 -0500

    Modify schema to ensure uniqueness of collectionid + userid. Create a function in the model to add a user to a collection. Create a function to remove a user from a collection

--- app/models/role.server.ts
+++ app/models/role.server.ts
@@ -2,29 +2,110 @@ import { CollectionRoles } from "@prisma/client";
 import { prisma } from "~/db.server";
 
 export const ROLE_TYPE = {
-    OWNER: "owner"
+  OWNER: "owner",
+  CONTRIBUTOR: "contributor"
+};
+
+type UserRole = "owner" | "contributor";
+
+export async function getRoleType(
+  actorId: string | undefined | null,
+  collectionId: string
+): Promise<string | null> {
+  if (!actorId) {
+    return null;
+  }
+  const role = await prisma.collectionRoles.findFirst({
+    where: {
+      userId: actorId,
+      collectionId: collectionId
+    }
+  });
+  if (!role) {
+    return null;
+  }
+  return role.role;
 }
 
-export async function getRoleType(actorId:string|undefined|null, collectionId:string):Promise<string|null> {
-    if (!actorId){
-        return null;
-    }
-    const role = await prisma.collectionRoles.findFirst({
-        where: {
-            userId: actorId,
-            collectionId: collectionId
-          },
-    });
-    if (!role){
-        return null;
-    }
-    return role.role;
+export async function getRolesTable(): Promise<CollectionRoles[]> {
+  const roles = await prisma.collectionRoles.findMany({
+    take: 100,
+    orderBy: { collectionId: "desc" }
+  });
+  return roles;
 }
 
-export async function getRolesTable():Promise<CollectionRoles[]>{
-    const roles = await prisma.collectionRoles.findMany({
-        take: 100,
-        orderBy: { collectionId: "desc" },
+export async function addUserToCollection(
+  userId: string,
+  collectionId: string,
+  role: UserRole
+): Promise<void> {
+  if (!userId) {
+    console.log("User ID is not provided.");
+  }
+
+  let assignedRole: string = "";
+
+  switch (role) {
+    case "owner":
+      assignedRole = ROLE_TYPE.OWNER;
+      break;
+    case "contributor":
+      assignedRole = ROLE_TYPE.CONTRIBUTOR;
+      break;
+    default:
+      console.log("The specified role does not exist.");
+      throw new Error("The specified role does not exist.");
+  }
+
+  try {
+    await prisma.collectionRoles.upsert({
+      where: {
+        collectionId_userId: {
+          collectionId: collectionId,
+          userId: userId
+        }
+      },
+      update: {
+        role: assignedRole
+      },
+      create: {
+        id: collectionId + userId,
+        collectionId: collectionId,
+        userId: userId,
+        role: assignedRole
+      }
     });
-    return roles;
+    console.log("User successfully added to the collection.");
+  } catch (error) {
+    console.error("Error adding user as owner:", error);
+    console.error(`Error adding user with role ${role}:`, error);
+  }
+}
+
+export async function removeUserFromCollection(
+  userId: string,
+  collectionId: string
+): Promise<void> {
+  if (!userId && !collectionId) {
+    console.log("User ID or Collection ID is not provided.");
+    return;
+  }
+
+  try {
+    await prisma.collectionRoles.delete({
+      where: {
+        collectionId_userId: {
+          collectionId: collectionId,
+          userId: userId
+        }
+      }
+    });
+    console.log("User successfully removed from the collection.");
+  } catch (error) {
+    console.error(
+      `Error removing user ${userId} from collection ${collectionId}:`,
+      error
+    );
+  }
 }
