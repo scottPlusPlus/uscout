3571580868aad5e67669353789a267682dbf20b5
Nick Imkamp <imkampn@gmail.com>
2023-09-10 15:51:41 -0500

    Add a delete action in admin page

--- app/routes/admin/roles.tsx
+++ app/routes/admin/roles.tsx
@@ -6,75 +6,95 @@ import { getRolesTable } from "~/models/role.server";
 import { requireUserId } from "~/session.server";
 
 export async function loader({ request, params }: LoaderArgs) {
-    const _ = await requireUserId(request);
-    const roles = await getRolesTable();
-    return json({ roles });
-};
+  const _ = await requireUserId(request);
+  const roles = await getRolesTable();
+  return json({ roles });
+}
 
-export default function AdminPage() {
+interface AdminPageProps {
+  rolesData: CollectionRoles[];
+}
 
-    const data = useLoaderData<typeof loader>();
-    const rolesData: CollectionRoles[] = !data ? [] : !data.roles ? [] : JSON.parse(JSON.stringify(data.roles));
+export default function AdminPage(props: AdminPageProps) {
+  const data = useLoaderData<typeof loader>();
+  const rolesData: CollectionRoles[] = !data
+    ? []
+    : !data.roles
+    ? []
+    : JSON.parse(JSON.stringify(data.roles));
 
-    const [sortBy, doSetSortBy] = useState('updated');
-    const setSortBy = (str: string) => {
-        doSetSortBy(str);
-    }
+  const [sortBy, doSetSortBy] = useState("updated");
+  const setSortBy = (str: string) => {
+    doSetSortBy(str);
+  };
 
-    const sortedEvents = () => {
-        console.log("sorting by " + sortBy);
-        const x = rolesData.sort((a, b) => {
-            switch (sortBy) {
-                case 'userId':
-                    return a.userId.localeCompare(b.userId);
-                default:
-                    return a.collectionId.localeCompare(b.collectionId);
-            }
-        });
-        return x;
-    };
+  const sortedEvents = () => {
+    console.log("sorting by " + sortBy);
+    const x = rolesData.sort((a, b) => {
+      switch (sortBy) {
+        case "userId":
+          return a.userId.localeCompare(b.userId);
+        default:
+          return a.collectionId.localeCompare(b.collectionId);
+      }
+    });
+    return x;
+  };
 
-    const renderRows = () => {
-        console.log("render rows...");
-        return sortedEvents().map((r) => (
-            <tr key={r.id}>
-                <td className="px-4 py-2">{r.collectionId}</td>
-                <td className="px-4 py-2">{r.userId}</td>
-                <td className="px-4 py-2">{r.role}</td>
+  const renderRows = () => {
+    console.log("render rows...");
+    return sortedEvents().map((r) => (
+      <tr key={r.id}>
+        <td className="px-4 py-2">{r.collectionId}</td>
+        <td className="px-4 py-2">{r.userId}</td>
+        <td className="px-4 py-2">{r.role}</td>
+        <td className="px-4 py-2">
+          <button
+            onClick={() => {
+              handleDelete(r.id);
+              console.log(r);
+            }}
+          >
+            <i className="fas fa-trash-alt"></i>
+          </button>
+        </td>
+      </tr>
+    ));
+  };
+
+  const handleDelete = (roleId: string) => {
+    // Handle the deletion logic here.
+    // For instance, you might make an API call to delete the role with the provided ID
+    console.log("Deleting role with ID:", roleId);
+  };
+
+  return (
+    <div>
+      <h3 className="text-2xl font-bold">Admin</h3>
+      <div className="flex flex-col">
+        <h2 className="mb-4 text-2xl font-bold">Roles Table</h2>
+        <table className="table-auto">
+          <thead>
+            <tr>
+              <th
+                className="cursor-pointer px-4 py-2"
+                onClick={() => setSortBy("collectionId")}
+              >
+                Collection
+              </th>
+              <th
+                className="cursor-pointer px-4 py-2"
+                onClick={() => setSortBy("userId")}
+              >
+                User
+              </th>
+              <th className="cursor-pointer px-4 py-2">Role</th>
+              <th className="px-4 py-2">Actions</th>{" "}
             </tr>
-        ));
-    };
-
-    return (
-        <div>
-            <h3 className="text-2xl font-bold">Admin</h3>
-            <div className="flex flex-col">
-                <h2 className="text-2xl font-bold mb-4">Roles Table</h2>
-                <table className="table-auto">
-                    <thead>
-                        <tr>
-                            <th
-                                className="px-4 py-2 cursor-pointer"
-                                onClick={() => setSortBy('collectionId')}
-                            >
-                                Collection
-                            </th>
-                            <th
-                                className="px-4 py-2 cursor-pointer"
-                                onClick={() => setSortBy('userId')}
-                            >
-                                User
-                            </th>
-                            <th
-                                className="px-4 py-2 cursor-pointer"
-                            >
-                                Role
-                            </th>
-                        </tr>
-                    </thead>
-                    <tbody>{renderRows()}</tbody>
-                </table>
-            </div>
-        </div>
-    );
+          </thead>
+          <tbody>{renderRows()}</tbody>
+        </table>
+      </div>
+    </div>
+  );
 }
